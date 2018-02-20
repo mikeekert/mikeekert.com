9eaedf587fd128f900ad96ee9d24eabb8f2cfbde
Mike Ekert <mikeekert@gmail.com>
2018-02-20 17:03:24 -0600

    [

--- src/app/landing/landing-main/landing-main.component.ts
+++ src/app/landing/landing-main/landing-main.component.ts
@@ -14,5 +14,34 @@ export class LandingMainComponent implements OnInit {
         $(this).height(h);
       });
     AOS.init();
+
+    $('a[href*="#"]')
+      .not('[href="#"]')
+      .not('[href="#0"]')
+      .click(function (event) {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+          let target = $(this.hash);
+          target = target.length
+            ? target
+            : $('[name=' + this.hash.slice(1) + ']');
+          if (target.length) {
+            event.preventDefault();
+            $('html, body').animate({
+              scrollTop: target
+                .offset()
+                .top
+            }, 1000, function () {
+              const $target = $(target);
+              $target.focus();
+              if ($target.is(':focus')) { // Checking if the target was focused
+                return false;
+              } else {
+                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                $target.focus(); // Set focus again
+              }
+            });
+          }
+        }
+      });
   }
 }
